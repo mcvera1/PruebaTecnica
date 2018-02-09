804d4e6a6e8672a2aff9bf8a290458528c5002bb
Unknown <vera975@gmail.com>
2018-02-09 10:23:41 -0500

    metodos para el tiempo de cada llamada

--- callCenterPrueba/src/main/java/callCenterPrueba/implement/Dispatcher.java
+++ callCenterPrueba/src/main/java/callCenterPrueba/implement/Dispatcher.java
@@ -1,9 +1,25 @@
 package callCenterPrueba.implement;
 
+import java.util.Vector;
+
+import callCenterPrueba.dto.TiempoLlamadaDTO;
 import callCenterPrueba.interfaz.DispatcherInterface;
+import callCenterPrueba.service.Tiempos;
 
 public class Dispatcher implements DispatcherInterface {
 
+	public void duracionLlamada(int cantidadLlamadas){
+		Tiempos tiempos = new Tiempos();
+		Vector tiemposLlamadas = new Vector();
+		TiempoLlamadaDTO tiemposLlamadasDto;
+		for(int i = 1; i <= cantidadLlamadas ; i++){
+			tiemposLlamadasDto = new TiempoLlamadaDTO();
+			tiemposLlamadasDto.setCodigoLlamada(i);
+			tiemposLlamadasDto.setDuraciónLlamada(tiempos.tiempoLlamada());
+			tiemposLlamadas.add(tiemposLlamadasDto);
+		}
+	}
+	
 	public void dispatcherCall() {
 		// TODO Auto-generated method stub
 		
--- callCenterPrueba/src/main/java/callCenterPrueba/service/Tiempos.java
+++ callCenterPrueba/src/main/java/callCenterPrueba/service/Tiempos.java
@@ -1,5 +1,12 @@
 package callCenterPrueba.service;
 
 public class Tiempos {
-
+	//metodo encargado de asignar el tiempo de llamada
+	public int tiempoLlamada(){
+	    int Max=10+1;// cota sup mas 1
+        int Min=5;
+        int tiempoLlamada = 0;
+        tiempoLlamada = ((int)(Math.random()*(Max-Min))+Min);
+        return tiempoLlamada;
+	}
 }
