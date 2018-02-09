0326f63b5afb4671df587983d930d1a4334732f6
vera <vera975@gmail.com>
2018-02-08 23:15:15 -0500

    Prueba tecnica

---
+++ callCenterPrueba/src/main/java/callCenterPrueba/callCenterPrueba/App.java
@@ -0,0 +1,13 @@
+package callCenterPrueba.callCenterPrueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
