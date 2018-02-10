c813ddf3cb19d871aa3b3fd9207307b2070ec257
vera <vera975@gmail.com>
2018-02-09 21:26:56 -0500

    comentarios

--- callCenterPrueba/src/main/java/callCenterPrueba/service/EstadoEmpleados.java
+++ callCenterPrueba/src/main/java/callCenterPrueba/service/EstadoEmpleados.java
@@ -1,7 +1,14 @@
 package callCenterPrueba.service;
 
 import java.util.Random;
-
+/**
+ * EstadoEmpleados
+ * @author vera
+ *metodo encargado de asignar un estado
+ *aleatorio a un empleado
+ *true=ocupado
+ *false=libre
+ */
 public class EstadoEmpleados {
     Random random = new Random();
     public boolean getBoolean() {
--- callCenterPrueba/src/main/java/callCenterPrueba/service/Tiempos.java
+++ callCenterPrueba/src/main/java/callCenterPrueba/service/Tiempos.java
@@ -1,7 +1,12 @@
 package callCenterPrueba.service;
 
 public class Tiempos {
-	//metodo encargado de asignar el tiempo de llamada
+	
+	/**
+	 *metodo encargado de asignar el tiempo aleatorio 
+	 *a la llamada entre 5 y 10 seg 
+	 * @return
+	 */
 	public int tiempoLlamada(){
 	    int Max=10+1;// cota sup mas 1
         int Min=5;
