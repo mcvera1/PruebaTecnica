429ddd2e44cad8bf28119df8d36374482026f92c
vera <vera975@gmail.com>
2018-02-09 21:32:34 -0500

    se agregan metodos al controller

--- callCenterPrueba/src/main/java/callCenterPrueba/controller/Controller.java
+++ callCenterPrueba/src/main/java/callCenterPrueba/controller/Controller.java
@@ -1,5 +1,22 @@
 package callCenterPrueba.controller;
 
+import callCenterPrueba.implement.Dispatcher;
+import callCenterPrueba.interfaz.DispatcherInterface;
+/**
+ * 
+ * @author vera
+ *metodo encargado para llamar cada uno de 
+ *los metodos que asignan las llamadas
+ */
 public class Controller {
-
+	
+	public static void main(String arg[]){
+		DispatcherInterface dispatcher = new Dispatcher();
+		final int cantidadEmpleados=10;
+		final int cantidadLlamadas = 10;
+		dispatcher.duracionLlamada(cantidadLlamadas);
+		dispatcher.cantidadEmpleado(cantidadEmpleados);
+		dispatcher.dispatcherCall();
+		dispatcher.llamadasEspera();
+	}
 }
